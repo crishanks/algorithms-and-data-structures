d1a081f5e6b5cd6d611cc3b8616b6d431d8ce5db
github username <crishanks12@gmail.com>
2019-05-14 12:32:29 -0600

    refactored to accept more than 2 names

--- wholikesit.js
+++ wholikesit.js
@@ -1,33 +1,30 @@
 function likes(names) {
-  console.log('names', names)
   if (names.length < 1) {
     return 'no one likes this'
   } else if (names.length === 1) {
     return `${names[0]} likes this`
   } else if (names.length === 2) {
-    return formatTwoNames(names);
+    return `${names[0]} and ${names[1]} like this`
   } else if (names.length === 3) {
-    return formatThreeNames(names);
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
   } else {
     return formatManyNames(names);
   }
 }
 
-function formatTwoNames(names) {
-  return `${names[0]} and ${names[1]} like this`
-}
-
-function formatThreeNames(names) {
-  for (let i = 0; i < names.length; i++) {
-    return `${names[0]}, ${names[1]} and ${names[2]} like this`
-  }
-}
-
 function formatManyNames(names) {
+  let result = '';
   for (let i = 0; i < names.length; i++) {
-    return `${names[i]}, ${names[i + 1]} and ${names.length - i} others like this`
+    const currentName = names[i];
+    if (i < 1) {
+      result += `${currentName}, `;
+    } else if (i === 1) {
+      result += currentName;
+    } else {
+      return result += ` and ${names.length - i} others like this`
+    }
   }
 }
 
-likes(['Alex', 'Jacob', 'Mark', 'Dave'])
-//'Alex, Jacob and 2 others like this'
+likes(['Alex', 'Jacob', 'Mark', 'Dave', 'Scott'])
+//'Alex, Jacob and 3 others like this'
