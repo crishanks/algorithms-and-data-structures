eb387adb6aaf8af9bc1b3fb3234b06e38d2c758f
github username <crishanks12@gmail.com>
2019-06-01 12:38:50 -0600

    more elegant solution

--- count-unique-values.js
+++ count-unique-values.js
@@ -2,17 +2,15 @@ function countUniqueValues(arr){
   if (arr.length === 0) {
     return 0;
   }
-  let uniqueValues = 1;
-  for (let i = 0; i < arr.length; i++) {
-    let j = i + 1;
-    if (j < arr.length) {
-      if (arr[i] !== arr[j]) {
-        uniqueValues++;
-      }
+
+let i = 0;
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
     }
-    j++;
   }
-  return uniqueValues;
+  return i + 1;
 }
 
 //sort the array
