b2397ba1dd7f103b38e347494f38547f2d20da1a
github username <crishanks12@gmail.com>
2019-06-04 12:11:18 -0600

    using sets one liner

--- duplicates-multiple-pointers.js
+++ duplicates-multiple-pointers.js
@@ -1,15 +1,23 @@
-function areThereDuplicates(...args) {
-  args.sort((a, b) => a > b);
-  let start = 0;
-  let next = 1;
-  for (let i = 0; i < args.length; i++) {
-    if (args[start] === args[next]) {
-      return true;
-    }
-    start++;
-    next++;
-  }
-  return false;
-}
+// function areThereDuplicates(...args) {
+//   args.sort((a, b) => a > b);
+//   let start = 0;
+//   let next = 1;
+//   for (let i = 0; i < args.length; i++) {
+//     if (args[start] === args[next]) {
+//       return true;
+//     }
+//     start++;
+//     next++;
+//   }
+//   return false;
+// }
 
-areThereDuplicates(1,2,3,2); //true;
+// areThereDuplicates(1,2,3,2); //true;
+
+//One Line Solution:
+
+function areThereDuplicates(...args) {
+  return new Set(arguments).size !== arguments.length;
+}
+ 
+areThereDuplicates(1,2,3); //false
