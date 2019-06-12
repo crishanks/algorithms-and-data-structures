89258b736a9c0862c868b8f0984c013f650ff807
github username <crishanks12@gmail.com>
2019-06-12 13:31:02 -0600

    more elegant solution using recursive helper

--- is-palindrome-recursive.js
+++ is-palindrome-recursive.js
@@ -1,13 +1,18 @@
 const isPalindrome = (str) => {
-  let reversed = '';
+  //reverse the string and return true if it matches str
+  //create a helper function that reverses the string
+  //grab the last letter of the word
+  //grab the rest of the word 
+  //if str.length <= 1 return str
+  //add the last letter to reverse(restOfString)
   const reverse = (str) => {
-  if (str.length <= 1) return str;
-  const lastLetter = str[str.length - 1];
-  const restOfWord = str.slice(0, str.length - 1);
-  return reversed += (lastLetter + reverse(restOfWord));
+    if (str.length <= 1) return str;
+    const lastLetter = str[str.length - 1];
+    const restOfString = str.slice(0, str.length - 1);
+    return lastLetter + reverse(restOfString);
   }
-  reverse(str);
-
-  if (reversed === str) return true;
+  if (reverse(str) === str) return true;
   return false;
 }
+
+isPalindrome('tacocat') //true
