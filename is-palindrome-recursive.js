87b2a656e62723a73fdc3175b85f161aa4eaa3d2
github username <crishanks12@gmail.com>
2019-06-13 15:17:48 -0600

    more elegant pure recursive solution

--- is-palindrome-recursive.js
+++ is-palindrome-recursive.js
@@ -1,18 +1,7 @@
-const isPalindrome = (str) => {
-  //reverse the string and return true if it matches str
-  //create a helper function that reverses the string
-  //grab the last letter of the word
-  //grab the rest of the word 
-  //if str.length <= 1 return str
-  //add the last letter to reverse(restOfString)
-  const reverse = (str) => {
-    if (str.length <= 1) return str;
-    const lastLetter = str[str.length - 1];
-    const restOfString = str.slice(0, str.length - 1);
-    return lastLetter + reverse(restOfString);
-  }
-  if (reverse(str) === str) return true;
-  return false;
+function isPalindrome(str) {
+  if (str.length === 0) return true;
+  if (str[0] !== str[str.length - 1]) return false;
+  return isPalindrome(str.slice(1, str.length - 1));
 }
 
-isPalindrome('tacocat') //true
+isPalindrome('tacocat'); //true
