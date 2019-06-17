03fa721abde27bc8f9350fd4aecb67d1fc40f67b
github username <crishanks12@gmail.com>
2019-06-17 09:39:54 -0600

    added shortcircuit if we go through w/o any swaps, we're done. no need to keep looping through

--- bubble-sort-nums.js
+++ bubble-sort-nums.js
@@ -1,10 +1,14 @@
 const bubbleSort = arr => {
+  let noSwaps;
   for (let i = arr.length - 1; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j < arr.length; j++) {
       if (arr[j] > arr[j+1]) {
         [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
   return arr;
 }
