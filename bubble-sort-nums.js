ee6e05921572f5f9b318aeb3e992720c839d12fa
github username <crishanks12@gmail.com>
2019-06-17 09:52:33 -0600

    sorting now doesnt go all the way through. last value of each iteration cemented

--- bubble-sort-nums.js
+++ bubble-sort-nums.js
@@ -2,7 +2,7 @@ const bubbleSort = arr => {
   let noSwaps;
   for (let i = arr.length - 1; i > 0; i--) {
     noSwaps = true;
-    for (let j = 0; j < arr.length; j++) {
+    for (let j = 0; j < i; j++) {
       if (arr[j] > arr[j+1]) {
         [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
         noSwaps = false;
@@ -12,5 +12,7 @@ const bubbleSort = arr => {
   }
   return arr;
 }
+
+bubbleSort([1,5,2,3,4]);
 
 bubbleSort([1,35,3,57,89,7,1,9,0]); //sort the nums from lowest to highest
