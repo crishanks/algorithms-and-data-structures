8350d234311fa9bfe63e5db8834964be84bc9399
github username <crishanks12@gmail.com>
2019-06-19 10:49:30 -0600

    created SinglyLinkedList class and added push instance method

--- linked-lists.js
+++ linked-lists.js
@@ -5,3 +5,23 @@ class Node {
   }
 }
 
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
