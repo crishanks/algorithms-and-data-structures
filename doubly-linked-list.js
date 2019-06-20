44148b33ef1e907ddb4dbb570d2847ef03aa28f0
github username <crishanks12@gmail.com>
2019-06-20 16:56:52 -0600

    unshift method dll

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -57,4 +57,18 @@ class DoublyLinkedList {
     return oldHead;
   }
 
+  unshift(val) {
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
 }
