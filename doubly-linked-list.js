5a22191737409569b7c2256c053ca519e451b1a9
github username <crishanks12@gmail.com>
2019-06-20 16:40:10 -0600

    pop method for dll

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -1,6 +1,6 @@
 class Node {
   constructor(val) {
-      this.val = val;
+    this.val = val;
     this.next = null;
     this.prev = null;
   }
@@ -16,15 +16,30 @@ class DoublyLinkedList {
   push(val) {
     const newNode = new Node(val);
     if (!this.head) {
-        this.head = newNode;
-        this.tail = newNode;
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-        this.tail.next = newNode;
-        newNode.prev = this.tail;
-        this.tail = newNode;
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
     }
     this.length++;
     return this;
   }
+
+  pop() {
+  if(!this.head) return null;
+  let poppedNode = this.tail;
+  if (this.length === 1) {
+    this.head = null;
+    this.tail = null;
+  } else {
+    this.tail = poppedNode.prev;
+    this.tail.next = null;
+    poppedNode.prev = null;  
+  }
+  this.length--;
+  return poppedNode;
+}
 
 }
