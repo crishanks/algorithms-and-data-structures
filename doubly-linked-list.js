04d12fa5684e126b1653584da52d7b0eb902844e
github username <crishanks12@gmail.com>
2019-06-20 16:23:22 -0600

    push method

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -1,6 +1,6 @@
 class Node {
-  costructor(val) {
-    this.val = val;
+  constructor(val) {
+      this.val = val;
     this.next = null;
     this.prev = null;
   }
@@ -12,4 +12,19 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+        this.head = newNode;
+        this.tail = newNode;
+    } else {
+        this.tail.next = newNode;
+        newNode.prev = this.tail;
+        this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
 }
