4a287365bd55816b9879d9b0d191267d51690f29
github username <crishanks12@gmail.com>
2019-07-10 17:01:36 -0600

    sock-merchant arrays and objects. O(n)`

---
+++ sock-merchant.js
@@ -0,0 +1,27 @@
+//John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+// For example, there are n=7 socks with colors ar=[1,2,1,2,1,3,2]. There is one pair of color 1 and one of color 2. There are three odd socks left, one of each color. The number of pairs is 2.
+
+function sockMerchant(n, ar) {
+  let matches = 0;
+  let matchesObj = {};
+
+  for (let i = 0; i < n; i++) {
+    const currentNum = ar[i];
+    const exists = matchesObj[currentNum];
+    if (!exists) {
+      matchesObj[currentNum] = 1;
+    } else {
+      matchesObj[currentNum] += 1;
+    }
+  }
+
+  for (let key in matchesObj) {
+    matches += Math.floor(matchesObj[key] / 2);
+  }
+
+  return matches;
+}
+
+sockMerchant(7, [1,2,1,2,1,3,2]);
+//2
