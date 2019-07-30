358c2ff934542899e21ff7e2f7cf203a7c4eef68
Cris Hanks <crishanks12@gmail.com>
2019-07-30 15:47:55 -0600

    queue with arrays, push and shift and unshift and pop

---
+++ queues.js
@@ -0,0 +1,18 @@
+//with an array
+const queue = [];
+
+//with push and shift
+queue.push('first')
+queue.push('second')
+queue.push('third')
+
+queue.shift()
+
+//with unshift and pop
+
+queue.unshift('1')
+queue.push('2')
+queue.push('3')
+
+queue.pop()
+
