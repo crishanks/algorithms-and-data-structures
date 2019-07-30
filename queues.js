7e2dfe84fc7cbfe6713e65ae5154f086c2310445
Cris Hanks <crishanks12@gmail.com>
2019-07-30 15:49:39 -0600

    set up Node and Queue classes

--- queues.js
+++ queues.js
@@ -16,3 +16,22 @@ queue.push('3')
 
 queue.pop()
 
+
+//with a linked list
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    
+}
