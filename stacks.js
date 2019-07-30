bc007439b78e6d77f874260af86aa0ef00598a7a
Cris Hanks <crishanks12@gmail.com>
2019-07-30 15:10:19 -0600

    imlplement stack with unshift and shift

--- stacks.js
+++ stacks.js
@@ -1,5 +1,6 @@
 let stack = [];
 
+//with push and pop
 stack.push("google")
 stack.push("instagram")
 stack.push("youtube")
@@ -7,3 +8,11 @@ stack.push("youtube")
 stack.pop()
 stack.push("amazon")
 stack.pop()
+
+
+//with unshift and shift
+stack.unshift('create new file')
+stack.unshift('resized file')
+stack.unshift('cloned out wrinkle')
+
+stack.shift()
