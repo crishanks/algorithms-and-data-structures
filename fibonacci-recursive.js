f4cea597ed72ebb638f07919ae3ee4d299f223fd
github username <crishanks12@gmail.com>
2019-08-09 20:19:04 -0600

    recursive solution to find the nth fib num WITH explanation

---
+++ fibonacci-recursive.js
@@ -0,0 +1,12 @@
+const fib = num => {
+  if (num <= 2) return 1;
+  return fib(num-1) + fib(num-2);
+}
+
+fib(6);
+
+//              fib(5)                        +                       fib(4)
+//        fib(3)      +        fib(2)                        fib(3)    +     fib(2)
+//    fib(2) + fib(1) -- fib(1) + fib(0)                fib(2) + fib(1) -- fib(1) fib(0)
+//      1    +   1    +    1    +   1         +           1    +   1    +    1  +   1  
+// = 8
