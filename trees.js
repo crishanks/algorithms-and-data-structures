52876c682c48be69bb482978d32c1c229c3ec087
github username <crishanks12@gmail.com>
2019-08-12 12:41:14 -0600

    insert method for binary search trees

--- trees.js
+++ trees.js
@@ -10,4 +10,31 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val) {
+    const newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let current = this.root;
+      while(true) {
+        if (val === current.value) return null;
+        if (val < current.val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }  
+    }
+  }
 }
