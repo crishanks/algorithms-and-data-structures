34ab3b3c5180cb473357cfee11d40b027f12fe50
github username <crishanks12@gmail.com>
2019-08-13 18:55:52 -0600

    added Math.floor to insure mid point is a whole num

--- merge-sort.js
+++ merge-sort.js
@@ -24,7 +24,7 @@ const merge = (arr1, arr2) => {
 
 const mergeSort = arr => {
   if (arr.length <= 1) return arr;
-  let mid = arr.length/2;
+  let mid = Math.floor(arr.length/2);
   let left = mergeSort(arr.slice(0, mid));
   let right = mergeSort(arr.slice(mid));
   return merge(left, right);
