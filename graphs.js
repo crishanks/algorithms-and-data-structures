5715115672bc12fd3e86802485d8a28357b138e4
github username <crishanks12@gmail.com>
2019-08-19 16:49:08 -0600

    remove this.removeEdge from removeVertex method

--- graphs.js
+++ graphs.js
@@ -9,7 +9,7 @@ class Graph {
 
   removeVertex(vertex) {
     while(this.adjacencyList[vertex].length) {
-      const adjacentVertex = this.removeEdge(this.adjacencyList[vertex].pop());
+      const adjacentVertex = this.adjacencyList[vertex].pop();
       this.removeEdge(vertex, adjacentVertex);
     }
     delete this.adjacencyList[vertex];
