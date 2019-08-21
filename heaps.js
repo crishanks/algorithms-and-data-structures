09a85d1838c4acb256a11c9821ce636ccbfad15c
github username <crishanks12@gmail.com>
2019-08-21 15:13:24 -0600

    max binary heap extract max and sink down methods

--- heaps.js
+++ heaps.js
@@ -20,5 +20,48 @@ class MaxBinaryHeap {
       childIndex = parentIndex;
     }
   }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end
+      this.sinkDown();
+    }
+    return max;
+  }
+
+  sinkDown() {
+    let parentIndex = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while(true) {
+      let leftChildIndex = 2 * (parentIndex - 1);
+      let rightChildIndex = 2 * (parentIndex - 2);
+      let leftChild, rightChild;
+      let swap = null;
+      
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > element) {
+          swap = leftChildIndex;
+        }
+      }
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild > element) || 
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+      if (swap === null) break;
+      this.values[parentIndex] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
+  }
   
 }
