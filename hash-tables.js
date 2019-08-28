c3da44bddd7c5774d97757f820924e7e70037398
github username <crishanks12@gmail.com>
2019-08-28 12:13:19 -0600

    hash table set method

--- hash-tables.js
+++ hash-tables.js
@@ -8,13 +8,26 @@ function hash(key, arrayLen) {
   return total;
 }
 
-function hash(key, arrayLen) {
-  let total = 0;
-  let WEIRD_PRIME = 31;
-  for (let i = 0; i < Math.min(key.length, 100); i++) {
-    let char = key[i];
-    let value = char.charChodeAt(0) - 96;
-    total = (total * WEIRD_PRIME + value) % arrayLen;
+class HashTable {
+  constructor(size=53) {
+    this.keyMap = new Array(size);
   }
-  return total;
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charChodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, value) {
+    const index = this._hash(key);
+    if (!this.keyMap[index]) this.keyMap[index] = [];
+    this.keyMap[index].push([key, value]);
+  }
+
 }
