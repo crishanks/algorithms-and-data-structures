fb787f7364f664f966d528372c7f7d2a4e8db47b
github username <crishanks12@gmail.com>
2019-08-28 11:58:59 -0600

    more optimized hash function

--- hash-tables.js
+++ hash-tables.js
@@ -1,8 +1,20 @@
+//Only takes strings, not constant time, not completely random distribution
 function hash(key, arrayLen) {
   let total = 0;
   for (let char of key) {
     let value = char.charcodeAt(0) - 96;
     total = (total + value) % arrayLen;
+  }
+  return total;
+}
+
+function hash(key, arrayLen) {
+  let total = 0;
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charChodeAt(0) - 96;
+    total = (total * WEIRD_PRIME + value) % arrayLen;
   }
   return total;
 }
